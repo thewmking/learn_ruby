9115d4a10e4bce667704fc90b844426f4401105a
Matthew King <thewmking@gmail.com>
2016-10-11 21:00:09 -0400

    my solutions

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
-#write your code here
+def echo (input)
+  input
+end
+
+def shout (input)
+  input.upcase
+end
+
+def repeat (input, number=2)
+  output = input
+  (number-1).times do
+      output += " " + input
+  end
+  output
+end
+
+def start_of_word (input, number=1)
+  input[0..(number-1)]
+end
+
+def first_word (input)
+  input.split.first
+end
+
+def titleize (input)
+  input.capitalize!
+  no_cap = ["an", "or", "and", "the", "over", "to"]
+  output = input.split(" ").map { |word|
+    if no_cap.include?(word)
+      word
+    else
+      word.capitalize
+    end
+  }.join(" ")
+  output
+end
